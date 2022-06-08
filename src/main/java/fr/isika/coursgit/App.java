5d06fc553f4bef78506d9858b96ae0f40cea4182
oliviavilliers <olivia.villiers@gmx.fr>
2022-06-08 22:03:57 +0200

    egit

---
+++ src/main/java/fr/isika/coursgit/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fr.isika.coursgit;
+
+/**
+ * @author Olivia
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
